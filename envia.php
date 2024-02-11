5e441cb716826f0d5d5e3c10afbb155aa44bdf4d
Cobick <159628561+Cobick@users.noreply.github.com>
2024-02-11 10:34:51 -0300

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+
+    $destino = "gvalentinsilva@gmail.com"
+    $assunto = "Formulario - Site"
+
+    $corpo = "Nome: ".$nome."\n"."E-mail ".$email."\n"."Telefone ".$telefone;
+
+    $cabecalho = "From: showbrman@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($destino, $assunto, $corpo, $cabecalho)){
+        echo("E-mail enviado com sucesso!");
+    }
+    else(mail($destino, $assunto, $corpo, $cabecalho)){
+        echo("Houve um erro ao enviar o e-mail")
+    }
+
+?>
